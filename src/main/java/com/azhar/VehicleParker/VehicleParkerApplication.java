aad98f69bb6a81daa261cccf33112d7e5f788adc
Azhar-k <azharazvtp@gmail.com>
2020-09-30 16:07:51 +0530

    initial commit

---
+++ src/main/java/com/azhar/VehicleParker/VehicleParkerApplication.java
@@ -0,0 +1,14 @@
+package com.azhar.VehicleParker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleParkerApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(VehicleParkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azhar/VehicleParker/VehicleParkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azhar.VehicleParker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleParkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
